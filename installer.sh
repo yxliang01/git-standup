17423f53495c7100547432a991a0827e265a19cf
Xiao Liang <yxliang01@users.noreply.github.com>
2018-06-07 15:09:48 +0800

    Using temporary folder as working directory -> Made install.sh more robust
    Previously, a new folder `git-standup` folder will be created in the working directory while the working directory can be anywhere which don't seem like a good way to do it.

--- installer.sh
+++ installer.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+## Create temporary folder
+dir=$(mktemp -d)
 ## Clone the repo
-git clone https://github.com/kamranahmedse/git-standup.git --depth=1 || { echo >&2 "Clone failed with $?"; exit 1; }
-cd git-standup
+git clone https://github.com/kamranahmedse/git-standup.git --depth=1 ${dir} || { echo >&2 "Clone failed with $?"; exit 1; }
+cd ${dir}
 make install || { echo >&2 "Clone failed with $?"; exit 1; }
 cd ..
-rm -rf git-standup
+rm -rf ${dir}
